98fd7ed7600840ece0a19f9c912a3224d64b8977
Kseniia Gladkova <gladkowaks@gmail.com>
2023-10-03 21:25:12 +0200

    adds buttons Marquer and Supprimer

--- client/frontend.js
+++ client/frontend.js
@@ -12,15 +12,28 @@ new Vue({
             contacts: []
         }
     },
+    // disable the button Enregistrer if empty
+    computed: {
+        canCreate() {
+            return this.form.value && this.form.name
+        }
+    },
     methods: {
         createContact() {
             // write the contact to an object
             const { ...contact } = this.form
             // adds the contact to the array and assign an id to the object
-            this.contacts.push({ ...contact, id: Date.now() })
+            this.contacts.push({ ...contact, id: Date.now(), marked: false });
             console.log(contact)
 
             this.form.name = this.form.value = ''
+        },
+        markContact(id) {
+            const contact = this.contacts.find(c => c.id === id)
+            contact.marked === true
+        },
+        removeContact(id) {
+
         }
     }
 })
