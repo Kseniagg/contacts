327c2f6f337c2b89173c9fe1bade93208b4c90ab
Kseniia Gladkova <gladkowaks@gmail.com>
2023-10-03 21:04:19 +0200

    creates a form and an object of contacts

--- client/frontend.js
+++ client/frontend.js
@@ -1,10 +1,28 @@
-import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js'
 
-createApp({
-    setup() {
-        const message = ref('Hello Vue!')
+
+new Vue({
+    el: '#app',
+    data() {
         return {
-            message
+            form: {
+                name: '',
+                value: ''
+            },
+            contacts: []
+        }
+    },
+    methods: {
+        createContact() {
+            // write the contact to an object
+            const { ...contact } = this.form
+            // adds the contact to the array and assign an id to the object
+            this.contacts.push({ ...contact, id: Date.now() })
+            console.log(contact)
+
+            this.form.name = this.form.value = ''
         }
     }
-}).mount('#app');
+})
+
+console.log(Vue);
